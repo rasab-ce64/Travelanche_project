bac3c700298aa86ce0548a65d05ac2d0a4147175
rasab-ce64 <33825130+rasab-ce64@users.noreply.github.com>
2018-01-27 17:09:32 +0500

    bids by other companies
    shfkl

--- application/views/company/Trips.php
+++ application/views/company/Trips.php
@@ -131,11 +131,12 @@
 
                 <!--panel footer-->
                 <div class="panel-footer">
+                    <h4 style="color: #000000">trip ID: <?php echo $row->id; ?></h4>
                     <h3>Total Bids: <?php  echo $row->bids_on_trip; ?> </h3>
                     <a type="submit" href="<?php echo site_url("Home/bachat/$row->id"); ?>"  class="btn btn-md" name="bid_now" >Bid by Bachat</a>
                     <a type="submit" href="<?php echo site_url("Home/lambSamb/$row->id"); ?>" class="btn btn-md" name="bid_now" >Bid by LambSamb</a>
 <!--                    <a type="button" class="btn btn-md" name="bid_now" data-toggle="modal" data-target="#modal" >Bid Now</a>-->
-                    <a type="button" class="btn btn-md" name="bid_now" data-toggle="modal" data-target="#modal" onclick="window.location='<?php echo site_url("company_logged_in/bids_By_Other_Companies/$row->id");?>'">Bids on Trip</a>
+                    <a type="button" class="btn btn-md" name="bid_now"  onclick="window.location='<?php echo site_url("company_logged_in/bids_By_Other_Companies/$row->id");?>'">Bids on Trip</a>
                     <input type="hidden" name="t_id" value="<?php echo $row->id; ?>"/>
                 </div>
             </div>
